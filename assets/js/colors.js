34f40ff6f9bdd1374c4952fbcba47ec8c26deedc
mariajantz <mkj8@pitt.edu>
2022-12-13 11:36:49 -0500

    try different height boxes

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,18 +1,17 @@
-genDivs(4)
-genDivs(4)
+genDivs(4, 'normal')
+genDivs(4, 'deut')
 
-function genDivs(cols){
+function genDivs(cols, rowname){
     var e = document.getElementById("target");
     var rows = 1
     for (var r = 0; r < rows; r++) {
         var row = document.createElement("div");
-        row.className = "normal";
+        row.className = rowname;
         for (var c = 0; c < cols; c++) {
             var col = document.createElement("div");
             let tmp = c+1;
             col.className = "columns project-" + tmp.toString();
             col.innerHTML = (r * rows) + c;
-            col.innerHTML = "A";
             row.appendChild(col);
         }
         e.appendChild(row);
