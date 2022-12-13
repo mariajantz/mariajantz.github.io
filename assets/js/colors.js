d2b00dcefc80dd3cf9faaf6647ea93592950693b
mariajantz <mkj8@pitt.edu>
2022-12-13 10:52:50 -0500

    add function js

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,8 +1,18 @@
-document.getElementById("test").innerHTML = 'replace text';
-console.log('hello world')
-document.getElementById("target").innerHTML = 'try here';
+genDivs(4)
 
 function genDivs(cols){
     var e = document.getElementById("target");
-
+    var rows = 1
+    for (var r = 0; r < rows; r++) {
+        var row = document.createElement("div");
+        row.className = "row";
+        for (var c = 0; c < cols; c++) {
+            var col = document.createElement("div");
+            col.className = "col";
+            col.innerHTML = (r * rows) + c;
+            col.innerHTML = "A";
+            row.appendChild(col);
+        }
+        e.appendChild(row);
+    }
 }
