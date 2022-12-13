a44aaf7dedda843dc644b44100147b35dc953ac0
mariajantz <mkj8@pitt.edu>
2022-12-13 11:06:57 -0500

    test align divs

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -5,7 +5,7 @@ function genDivs(cols){
     var rows = 1
     for (var r = 0; r < rows; r++) {
         var row = document.createElement("div");
-        row.className = "row";
+        row.className = "columns";
         for (var c = 0; c < cols; c++) {
             var col = document.createElement("div");
             col.className = "col";
