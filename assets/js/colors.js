63af3d33b6bb528ec119481860e92513f7d1bbf2
mariajantz <mkj8@pitt.edu>
2022-12-13 13:20:23 -0500

    test

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -13,7 +13,7 @@ function genDivs(cols, rowname){
         var lbl = document.createElement("div");
         lbl.className = "columns lbl";
         lbl.innerHTML = rowname;
-        lbl.style.width = '10%';
+        lbl.style.width = '100px';
         row.appendChild(lbl)
         for (var c = 0; c < cols; c++) {
             var col = document.createElement("div");
