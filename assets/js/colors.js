278d5bf756995185fe851dfe383ec8fade644780
mariajantz <mkj8@pitt.edu>
2022-12-14 14:03:18 -0500

    change colors

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,46 +1,7 @@
 var num_divs = document.getElementById('num_clrs').value
 
-// genDivs(num_divs, 'normal', 'normal')
-// genDivs(num_divs, 'colorblind', 'deuteranopia')
-// genDivs(num_divs, 'colorblind', 'protanopia')
-// genDivs(num_divs, 'colorblind', 'tritanopia')
 console.log(num_divs);
 genDivsGrid(4);
-
-function genDivs(cols, rowname, rowid){
-    var e = document.getElementById("target");
-    var rows = 1
-    for (var r = 0; r < rows; r++) {
-        var row = document.createElement("div");
-        row.className = rowname;
-        row.id = rowid; 
-        var fullwd = row.style.width; 
-        console.log(fullwd)
-        //before starting, make a div with a label
-        var lbl = document.createElement("div");
-        lbl.className = "columns lbl";
-        lbl.innerHTML = rowid;
-        lbl.style.width = '100px';
-        row.appendChild(lbl)
-        for (var c = 0; c < cols; c++) {
-            var col = document.createElement("div");
-            let tmp = c+1;
-            col.className = "columns project-" + tmp.toString();
-            //col.innerHTML = (r * rows) + c;
-            //CSS changes: 
-            //change width of these boxes to fill the space
-            cwidth = Math.floor((fullwd-100)/cols-2*(cols+1)*(fullwd/200)); 
-            col.style.width = cwidth.toString() + 'px';
-            //col.style.width = cwidth.toString() + '%';
-            //then put a color here
-            col.style.backgroundColor = genColor();
-            //col.style.display = 'grid'; 
-            //if rowname isn't "normal" then recast this to the correct type
-            row.appendChild(col);
-        }
-        e.appendChild(row);
-    }
-}
 
 function genDivsGrid(cols) {
     var e = document.getElementById("target");
@@ -93,7 +54,7 @@ function updateColors() {
     //if not, delete any extra divs (last child in each row of target) and resize all of them 
     //if it is the same number, regenerate each child element
     //get all the existing divs in target - normal, then update the colors (for now use random colors)
-    var normblocks = document.getElementById("normal").childNodes;
+    var normblocks = document.getElementById("target").childNodes;
     
     for (var c=1; c<normblocks.length; c++) {
         normblocks[c].style.backgroundColor = genColor();
