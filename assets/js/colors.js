2be0358738178b5ea1ceb2c496089b905cf5d030
mariajantz <mkjantz@gmail.com>
2022-12-16 21:36:28 -0500

    also log all elements

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -181,6 +181,7 @@ function rmColumns(new_colCount) {
     const elements = document.getElementsByClassName(' col-' + c.toString());
     while (elements.length > 0) {
         console.log(elements)
+        console.log(normblocks)
         elements[0].parentNode.removeChild(elements[0]);
         //probably need to also remove sub children? 
     }
