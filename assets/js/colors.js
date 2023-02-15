2ad1fe8d390e461f61a22e6882bb78b6e794bbf9
mariajantz <mkj8@pitt.edu>
2023-02-15 12:04:00 -0500

    inline median calculation

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -280,7 +280,7 @@ function sortColors(clr_list, ref_clrs) {
     let x = 2; 
     //find distances between everything
     var cdist = [] //change below to i<all_clrs.length
-    var cmedians = []
+    
     for (var i=0; i<5; i++){
         cdist.push([])
         //todo this is non symmetrical why
@@ -301,12 +301,13 @@ function sortColors(clr_list, ref_clrs) {
         //calculate the median distances in each row, and if there are any values in the row less than 10
         //they are not likely to be distinguishable so subtract from median to move it down the list?
         //but that should only apply to one of a pair
-        cmedians.push(median(cdist[i]));
+        //cmedians.push(median(cdist[i]));
         let tmpArr = cdist[i].slice(0, i).concat(cdist[i].slice(-(cdist[i].length - i - 1))); //ignore 0s on diagonal
         const closeClr = cdist[i].indexOf(Math.min(...tmpArr)); 
         console.log(closeClr);
 
     }
+    var cmedians = cdist.map(x => median(x));
     console.log('median')
     console.log(cmedians)
     console.log(sortIndex(cmedians))
